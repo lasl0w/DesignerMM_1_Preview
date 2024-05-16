4eefcb04569dc65509c413439b124bed8ac7f94a
Tom Montgomery <montgomery3681@gmail.com>
2024-05-15 22:46:28 -0600

    Initial Commit

---
+++ DesignerMM_1_Preview/DesignerMM_1_PreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignerMM_1_PreviewApp.swift
+//  DesignerMM_1_Preview
+//
+//  Created by tom montgomery on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignerMM_1_PreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
